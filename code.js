45138bc68abec842a598f478ff81789c707ca636
ceREAl <rlejko13@gmail.com>
2021-05-19 17:37:45 +0300

    updated code.js

--- code.js
+++ code.js
@@ -1,50 +1,37 @@
 let showKeyTrue = document.getElementById('showKeyTrue');
 const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234key6789%^&"
 
-let iq = 2;
-let jq = 9;
+let key = randomValueInitializator();
 
-let key = keyInitializator();
-
-function keyInitializator() { 
-    for (let i = 0; i < 1; i++){
-        return possible.charAt(Math.floor(Math.random() * possible.length))
-         
-    }
+function randomValueInitializator() { 
+    return possible.charAt(Math.floor(Math.random() * possible.length))
 }
 
 console.log(key);
 
 let arr = [];
-    min = 0;
-    max = 99;
 
-
-for (let i = 0; i<11; i++){  //vertical quantity
-    arr[i] = [null]
-    for (let j = 0; j < 11; j++) {  //horizontal quantity
-        
-        arr[i][j] = possible.charAt(Math.floor(Math.random() * possible.length));
-        
-        if (i === iq && j === jq) {
-            arr[i][j] = key;
-
-            iq++;
-            jq--;
-        }        
+for (let i = 0; i < 10; i++){  //vertical quantity
+    arr[i] = [];
+    for (let j = 0; j < 10; j++) {  //horizontal quantity
+        if (i + j === 9 && j !== 9) {
+            arr[i][j] = key
+        } else{
+            arr[i][j] = randomValueInitializator();
+        }
     }
 }
 
 let counter = 99;
 document.write ('<table width="100%" border="4">');
-for (i=1; i<=11; i++) {
+for (i=0; i<=10; i++) {
     document.writeln("<tr>");
     
-    for (j=1; j<11; j++){ ;
+    for (j=0; j<10; j++){ ;
         document.write("<td>" + `${counter} __ ${arr[i][j]}` + "<\/td>")
         counter--;
     }
-      document.writeln("<\/tr>");
+        document.writeln("<\/tr>");
 }
 
 document.write ("<\/table> ");
